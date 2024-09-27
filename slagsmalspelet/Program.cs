0568371acbe7dbe455746d31c69e9af211330557
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2024-09-27 14:29:49 +0200

    klar!!!!

--- slagsmalspelet/Program.cs
+++ slagsmalspelet/Program.cs
@@ -1,28 +1,29 @@
 ﻿
 using System.Net.Security;
+string skurknamn;
 
-int hjältehp = 100 ;
-int skurkhp = 100 ;
 
+int hjältehp = 100;
+int skurkhp = 100;
 
 
 Console.WriteLine("Skriv in ditt namn");
-string hjältenamn  = Console.ReadLine();
-string skurknamn   = "";
+string hjältenamn = Console.ReadLine();
 Console.WriteLine($" Hej {hjältenamn}");
 
-string enemyPic = "";
+string enemyPic;
 
-
- Console.WriteLine("Tryck för att veta vem din fiende är");
+Console.Clear();
+Console.WriteLine("Tryck för att veta vem din fiende är");
 Console.ReadLine();
 
 
+// Slumpar vilken fiende man får 
 
 if (Random.Shared.Next(2) == 0)
 {
-   enemyPic = """
-            c~~p ,---------.
+      enemyPic = """
+            c~~p,---------.
       ,---'oo  )           \
       ( O O                  )/
       `=^='                 /
@@ -31,15 +32,13 @@ if (Random.Shared.Next(2) == 0)
             ||__|    |_|__|
 
       """;
-      Console.WriteLine("Moo deng");
-      skurknamn = Console.ReadLine();
+      skurknamn = "Moo deng";
 
 }
-      
+
 else
 {
-
-enemyPic = """
+      enemyPic = """
 
         _
        / \      _-'
@@ -57,76 +56,62 @@ __-' { |          \
       _-'
 
 """;
-Console.WriteLine("Alpha sigma");
-skurknamn =Console.ReadLine();
-
-
+      skurknamn = "Alpha sigma";
 }
 
 
 Console.Clear();
- new Random();
+new Random();
 
 while (hjältehp > 0 && skurkhp > 0)
 
 {
-Console.WriteLine(enemyPic);
+      Console.WriteLine(enemyPic);
 
-Console.WriteLine("\n NY RUNDA");
-Console.WriteLine($"{hjältenamn}: {hjältehp} {skurknamn}: {skurkhp}\n ");
+      Console.WriteLine("\n NY RUNDA");
+      Console.WriteLine($"{hjältenamn}: {hjältehp} {skurknamn}: {skurkhp}\n ");
 
-int skada1 = Random.Shared.Next(20);
-skurkhp -= skada1;
-skurkhp = Math.Max(0,skurkhp);
-Console.WriteLine($"{skurknamn}gör {skada1} skada på {hjältenamn}");
+      int skada1 = Random.Shared.Next(50);
+      skurkhp -= skada1;
+      skurkhp = Math.Max(0, skurkhp);
+      Console.WriteLine($"{skurknamn}gör {skada1} skada på {hjältenamn}");
 
 
-int skada2 = Random.Shared.Next(20);
-hjältehp -= skada2;
-hjältehp= Math.Max(0,hjältehp);
-Console.WriteLine($"{hjältenamn} gör {skada2} skada på {skurknamn}");
+      int skada2 = Random.Shared.Next(50);
+      hjältehp -= skada2;
+      hjältehp = Math.Max(0, hjältehp);
+      Console.WriteLine($"{hjältenamn} gör {skada2} skada på {skurknamn}");
 
-Console.WriteLine ("tryck nånstans för att fortsätta.");
-Console.ReadKey();
-
+      Console.WriteLine("tryck nånstans för att fortsätta.");
+      Console.ReadKey();
 
 }
 
 
 Console.WriteLine("\n striden är slut");
 
-if(hjältehp == 0 && skurkhp == 0)
+if (hjältehp == 0 && skurkhp == 0)
 
 {
-Console.WriteLine("Oavgjort");
-Console.WriteLine("):");
-Console.ReadLine();
+      Console.WriteLine("Oavgjort");
+      Console.WriteLine("):");
+      Console.ReadLine();
 }
 
 
 if (hjältehp == 0)
 
 {
-Console.WriteLine($"{skurknamn} vann");
-Console.WriteLine("""
-
-      c~~p ,---------.
- ,---'oo  )           \
-( O O                  )/
- `=^='                 /
-       \    ,     .   /
-       \\  |-----'|  /
-       ||__|    |_|__|
-
-""");
-Console.ReadLine();
+      Console.WriteLine($"{skurknamn} vann");
+      Console.WriteLine($"{enemyPic}");
+      Console.ReadLine();
 }
 
 if (skurkhp == 0)
 
 {
-Console.WriteLine($"{hjältenamn} vann");
-Console.WriteLine("""
+      Console.WriteLine($"{hjältenamn} vann");
+      Console.WriteLine("""
 
                 \||/
                 |  @___oo
@@ -144,11 +129,10 @@ Console.WriteLine("""
 
 """);
 
-Console.ReadLine();
+      Console.ReadLine();
 }
 
 Console.WriteLine("Tryck på valfri knapp för att avsluta.");
-
 Console.ReadKey();
 
 
